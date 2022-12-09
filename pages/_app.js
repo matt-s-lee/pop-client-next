8ab4ae473d507f80457f66fc9784e3b074c901ff
Matt <m.msklee@gmail.com>
2022-12-09 15:20:02 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { LanguageProvider } from "../context/LanguageContext";
+
+import Layout from "../components/Layout";
+
+import "../styles/globals.css";
+// import { Inter } from "@next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LanguageProvider>
+      <Layout />
+      {/* <main className={inter.className}> */}
+      <Component {...pageProps} />
+      {/* </main> */}
+      {/* </Layout> */}
+    </LanguageProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
