6c5d8c767cf0de1d10ca3dbb2ea6a7ab2a46e935
Matt <m.msklee@gmail.com>
2022-12-14 11:58:28 -0500

    Install Babel plugin for styled-components

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+// To wait for styled-components to load
+// https://stackoverflow.com/questions/61378312/wait-for-css-to-load-on-a-nextjs-static-site
+
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
