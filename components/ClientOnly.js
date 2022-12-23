94970f6d4d1b963fa86b2911ac566ba6e876bccb
Matt <m.msklee@gmail.com>
2022-12-23 00:08:17 -0500

    Fix MUI className error on hydration

---
+++ components/ClientOnly.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+// Function to render component on client-side
+export default function ClientOnly({ children, ...delegated }) {
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+  return <div {...delegated}>{children}</div>;
+}
