201341cffde8b77acfb1a99737722c516c3c4c83
Matt <m.msklee@gmail.com>
2022-12-22 23:59:49 -0500

    feat: Fix slug creation in categories pages

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,14 +1,19 @@
-import { useRouter } from "next/router";
+import { useState } from "react";
+
+// import { useRouter } from "next/router";
 
 export default function CategoryPage({ category }) {
-  const router = useRouter();
-  const slug = router.query.slug;
-  const locale = router.locale;
-
-  return <div>{category.fields.sectionTitle[locale]}</div>;
+  return (
+    <div>
+      <>{category.fields.sectionTitle}</>
+      <div>{category.fields.description.content[0].content[0].value}</div>
+      <div></div>
+    </div>
+  );
 }
 
-export async function getStaticPaths(props) {
+// export async function getStaticPaths({ locales }) {
+export async function getStaticPaths() {
   // When in preview environments, don't prerender any static pages
   if (process.env.SKIP_BUILD_STATIC_GENERATION) {
     return {
@@ -17,26 +22,22 @@ export async function getStaticPaths(props) {
     };
   }
 
-  // Get all categories from Contentful API
+  // Fetch all categories in English, from Contentful API
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=sectionTest&locale=*`
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=en-CA`
   );
   const categories = await res.json();
 
   // Create array of slugs from objects, i.e. {locale: slug}
   const allSlugs = [];
   categories.items.forEach((category) => {
-    let slugs = Object.values(category.fields.slug);
-    slugs.forEach((slug) => {
-      allSlugs.push(slug);
-    });
+    allSlugs.push(category.fields.slug);
   });
 
   // Get slug paths to prerender
   const paths = allSlugs.map((slug) => {
     return { params: { slug } };
   });
-  console.log(paths);
 
   return {
     paths,
@@ -47,12 +48,31 @@ export async function getStaticPaths(props) {
 export async function getStaticProps(context) {
   // Get one category from Contentful API
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=sectionTest&locale=*&fields.slug[in]=${context.params.slug}`
+    // `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=en-CA`
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=en-CA&fields.slug[in]=${context.params.slug}`
   );
   const category = await res.json();
 
-  // "metadata", "sys" and "fields" passed to the page component as props
+  // Passed to the CategoryPage component as props
   return {
     props: { category: category.items[0] },
   };
 }
+
+// console.log("category page props", categories);
+
+// return category.items.map((category) => {
+//   return (
+//     <div key={category.fields.sectionTitle}>
+//       <>{category.fields.sectionTitle}</>
+//       <div>{category.fields.description.content[0].content[0].value}</div>
+//       <div></div>
+//     </div>
+//   );
+// });
+
+// const [categoryData, setCategoryData] = useState();
+// setCategoryData(categories.items);
+// const router = useRouter();
+// const slug = router.query.slug;
+// const locale = router.locale;
