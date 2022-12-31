1321c5b25052d3c89f9bc7735f27f889f5a4f448
Matt <m.msklee@gmail.com>
2022-12-30 23:36:54 -0500

    Move fetch to app Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,31 @@
+import { useContext, useEffect } from "react";
+import { CategoriesContext } from "../context/CategoriesContext";
+
 import Footer from "./Footer";
 import Navbar from "./Navbar/index";
 import Navigator from "./Navigator";
 
 export default function Layout({ children }) {
+  const {
+    setCategories,
+    setTopics,
+    setSupportTypes,
+    setSupportTypesNav,
+    setProvinces,
+  } = useContext(CategoriesContext);
+
+  useEffect(() => {
+    fetch("/api/navbar")
+      .then((response) => response.json())
+      .then((json) => {
+        setProvinces(json.provincesJson);
+        setSupportTypes(json.supportTypesJson);
+        setSupportTypesNav(json.supportTypesNavJson);
+        setTopics(json.categoriesNavJson);
+        setCategories(json.categoriesJson);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
