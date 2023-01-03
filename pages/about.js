be1675cbdb65251f71a66924e93ed0a0f288212e
Matt <m.msklee@gmail.com>
2023-01-03 16:33:47 -0500

    Add sections to About page

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+
 export default function About() {
-  return <div>about</div>;
+  return (
+    <Wrapper>
+      <h1>About</h1>
+      <Text>Text</Text>
+      <Logos>Logos</Logos>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  padding: 1em;
+`;
+
+const Text = styled.div``;
+
+const Logos = styled.div``;
