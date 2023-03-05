2c3084a44d71092c8c8c79c3463c163ed980b878
Matt <m.msklee@gmail.com>
2023-03-05 15:56:19 -0500

    feat: Create feedback form

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,5 +1,79 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import styled from "styled-components";
 
 export default function Feedback() {
-  return <div></div>;
+  const form = useRef();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const handleInputChange = (ev, setter) => {
+    setter(ev.target.value);
+  };
+
+  const sendEmail = (ev) => {
+    ev.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_517bv4x",
+        "template_w4a9ung",
+        form.current,
+        "XHYAs1hCbfTWVC5FZ"
+      )
+      .then(
+        (result) => {
+          console.log("SUCCESS!", result);
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+  };
+
+  return (
+    <Container>
+      <div>Please fill out the form below to send any feedback.</div>
+      <Form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="user_name"
+          value={name}
+          onChange={(ev) => handleInputChange(ev, setName)}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          name="user_email"
+          value={email}
+          onChange={(ev) => handleInputChange(ev, setEmail)}
+        />
+        <label>Message</label>
+        <textarea
+          name="message"
+          value={message}
+          onChange={(ev) => handleInputChange(ev, setMessage)}
+        />
+        <input type="submit" value="Send" />
+      </Form>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
