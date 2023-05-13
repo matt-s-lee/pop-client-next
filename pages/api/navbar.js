051718921c13eef0defb5a17e3bf0057179ef975
Matt <m.msklee@gmail.com>
2023-05-13 09:46:39 -0400

    chore: Add comments

--- pages/api/navbar.js
+++ pages/api/navbar.js
@@ -1,20 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// fetch("/api/navbar") called in Layout.js
 
 export default async function handler(req, res) {
   const categoriesAll = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=en-CA&order=fields.categoryOrderNumber`
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=*&order=fields.categoryOrderNumber`
   );
+  // Ordered for categories (for navigation bar) - ID number only
   const categoriesNav = await fetch(
     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries/6mA0dNkT5wRCCpEgOIWvlG?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
   );
   const supportTypesAll = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=supportType&order=fields.orderNumber`
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=supportType&locale=*&order=fields.orderNumber`
   );
+  // Order for support types (for navigation bar) - ID number only
   const supportTypesNav = await fetch(
     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries/IHIT9WDkE1WaV5nnszNLP?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
   );
+  //
   const provinces = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=provinceSpecificResourceList&order=fields.orderNumber`
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=provinceSpecificResourceList&order=fields.orderNumber&locale=*`
   );
 
   const categoriesJson = await categoriesAll.json();
@@ -32,49 +36,3 @@ export default async function handler(req, res) {
     provincesJson,
   });
 }
-
-// const data = Promise.all([
-//   // Categories (in order)
-//   fetch(
-//     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=en-CA&order=fields.categoryOrderNumber`
-//   ),
-//   // Topics for navigation bar
-//   fetch(
-//     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries/6mA0dNkT5wRCCpEgOIWvlG?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
-//   ),
-//   // Support types for navigation bar
-//   fetch(
-//     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries/IHIT9WDkE1WaV5nnszNLP?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
-//   ),
-//   // All support types
-//   fetch(
-//     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=supportType&order=fields.orderNumber`
-//   ),
-//   // Provincial resource lists
-//   fetch(
-//     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=provinceSpecificResourceList&order=fields.orderNumber`
-//   ),
-// ]);
-// -----------------------
-// 1 - clg = {}; api = {}
-// -----------------------
-// const data = fetch(
-//   `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=categories&locale=en-CA&order=fields.categoryOrderNumber`
-// );
-// data
-//   .then((response) => {
-//     response.json();
-//   })
-//   .then((json) => {
-//     console.log("json", json);
-//     return json;
-//   });
-
-// -----------------------------
-// 2 - clg = ok; api = {}
-// -----------------------------
-// const data = await fetch(
-//   `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=provinceSpecificResourceList&order=fields.orderNumber`
-// );
-// const response = await data.json();
-// res.json(response); // api doesn't resolve without
